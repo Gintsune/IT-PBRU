35f220462fc8b97ee9c15e4c92ef32c51dbe72b1
Gin <kingdomheartii@hotmail.com>
2016-06-07 16:00:33 +0700

    Delete ALLSQLite

--- app/src/main/java/com/example/lab324_25/itpbru/MainActivity.java
+++ app/src/main/java/com/example/lab324_25/itpbru/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.lab324_25.itpbru;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,7 +20,17 @@ public class MainActivity extends AppCompatActivity {
         // Test Add New User
         //myManage.addNewUser("123","name", "sur", "user", "pass");
 
+        //Delete All SQLite
+        deleteAllSQLite();
+
     }//Main Method
+
+    private void deleteAllSQLite() {
+
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+        sqLiteDatabase.delete(MyManage.user_table, null, null);
+    }   // deleteAllSQLite
 
     public void clickSignUpMain(View view) {
         startActivity(new Intent(MainActivity.this, SignUpActivity.class));
